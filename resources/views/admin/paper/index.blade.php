881cada4d24857705c4181840dcc7691ca20b98a
alif <alifadiawan2005@gmail.com>
2024-10-01 14:27:30 +0700

    new:dashboard sedikit

--- resources/views/admin/paper/index.blade.php
+++ resources/views/admin/paper/index.blade.php
@@ -1,4 +1,25 @@
 @extends('layout-admin.main')
 @section('content')
-        
+      
+
+<div class="row">
+    <div class="col">
+        <table class="table table-borderless">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Full Name</th>
+                    <th>Country of Residence</th>
+                    <th>Profesion</th>
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                    <th>Student Number</th>
+                    <th>Nationality</th>
+                    <th>Proof</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+
 @endsection
